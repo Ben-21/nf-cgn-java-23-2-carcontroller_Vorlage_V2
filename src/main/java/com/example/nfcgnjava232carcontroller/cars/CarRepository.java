f3b02222ab4896c8bf62cdf8cbba51ce899ac795
Christian Weck <weck.chr@gmail.com>
2023-06-13 10:24:06 +0200

    CarRepository instantiates cars as ArrayList

--- src/main/java/com/example/nfcgnjava232carcontroller/cars/CarRepository.java
+++ src/main/java/com/example/nfcgnjava232carcontroller/cars/CarRepository.java
@@ -1,10 +1,15 @@
 package com.example.nfcgnjava232carcontroller.cars;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarRepository {
 
     private List<Car> cars;
+
+    public CarRepository() {
+        this.cars = new ArrayList<>();
+    }
 
     public List<Car> addCar(Car car) {
         this.cars.add(car);
