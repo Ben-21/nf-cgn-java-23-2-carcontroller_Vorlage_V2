39c9ad65ff1101f6b906a266d6f15ee369f5559e
Christian Weck <weck.chr@gmail.com>
2023-06-13 10:30:48 +0200

    added empty, all args Constructor, added getters and setters

--- src/main/java/com/example/nfcgnjava232carcontroller/cars/Car.java
+++ src/main/java/com/example/nfcgnjava232carcontroller/cars/Car.java
@@ -6,4 +6,37 @@ public class Car {
     private int numberOfTires;
     private boolean hasMot;
 
+    public Car() {
+
+    }
+
+    public Car(String brand, int numberOfTires, boolean hasMot) {
+        this.brand = brand;
+        this.numberOfTires = numberOfTires;
+        this.hasMot = hasMot;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getNumberOfTires() {
+        return numberOfTires;
+    }
+
+    public boolean isHasMot() {
+        return hasMot;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setNumberOfTires(int numberOfTires) {
+        this.numberOfTires = numberOfTires;
+    }
+
+    public void setHasMot(boolean hasMot) {
+        this.hasMot = hasMot;
+    }
 }
