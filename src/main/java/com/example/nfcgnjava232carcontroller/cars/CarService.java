a78af4b7deb9899aaadb2f9028ad21ad033b6237
Muslim Kareem <muslimkareem@icloud.com>
2023-06-13 16:09:44 +0200

    Small change in CarService

--- src/main/java/com/example/nfcgnjava232carcontroller/cars/CarService.java
+++ src/main/java/com/example/nfcgnjava232carcontroller/cars/CarService.java
@@ -1,6 +1,5 @@
 package com.example.nfcgnjava232carcontroller.cars;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,13 +9,15 @@ public class CarService {
     private CarRepository carRepository;
 
 
-
     public CarService(CarRepository carRepository) {
         this.carRepository = carRepository;
     }
 
-    public List<Car> addCar(Car car) {
-        return this.carRepository.addCar(car);
+    public List<Car> addCarInService(Car car) {
+        if(car.getBrand().equals("BMW")){
+            this.carRepository.addCarInRepo(car);
+        }
+        return this.carRepository.getCars();
     }
 
     public List<Car> getCars() {
