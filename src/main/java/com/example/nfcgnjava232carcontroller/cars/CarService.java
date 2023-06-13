20ff881a3ee79844c7721c836a62c6d20dfb1434
Christian Weck <weck.chr@gmail.com>
2023-06-13 10:06:36 +0200

    added CarService

---
+++ src/main/java/com/example/nfcgnjava232carcontroller/cars/CarService.java
@@ -0,0 +1,16 @@
+package com.example.nfcgnjava232carcontroller.cars;
+
+import java.util.List;
+
+public class CarService {
+
+    private CarRepository carRepository;
+
+    public List<Car> addCar(Car car) {
+        return this.carRepository.addCar(car);
+    }
+
+    public List<Car> getCars() {
+        return this.carRepository.getCars();
+    }
+}
