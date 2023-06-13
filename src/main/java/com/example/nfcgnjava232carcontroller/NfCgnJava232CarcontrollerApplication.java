7f6b71b7fa80b96f5a2992d3e7d1f5bc2fba5bc3
Christian Weck <weck.chr@gmail.com>
2023-06-13 09:58:48 +0200

    init: First Commit

---
+++ src/main/java/com/example/nfcgnjava232carcontroller/NfCgnJava232CarcontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.example.nfcgnjava232carcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfCgnJava232CarcontrollerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfCgnJava232CarcontrollerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nfcgnjava232carcontroller/NfCgnJava232CarcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nfcgnjava232carcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfCgnJava232CarcontrollerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
